a05dc3f80d51671edc9291b069eb820516987971
WilliamNeasbey <108777720+WilliamNeasbey@users.noreply.github.com>
2023-06-22 15:42:28 +1000

    AI roaming an title screen fix

--- Assets/SCripts/FollowPlayer.cs
+++ Assets/SCripts/FollowPlayer.cs
@@ -6,16 +6,54 @@ using UnityEngine.AI;
 public class FollowPlayer : MonoBehaviour
 {
     public Transform target;
+    public Animator animator;
     NavMeshAgent nav;
+
+    private bool isWalking = false;
+    private bool playerIsMoving = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        nav = GetComponent<NavMeshAgent>();   
+        nav = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        nav.updateRotation = false; // Disable NavMeshAgent rotation
     }
 
     // Update is called once per frame
     void Update()
     {
-        nav.SetDestination(target.position);
+        if (nav.remainingDistance > nav.stoppingDistance)
+        {
+            if (!isWalking)
+            {
+                animator.Play("Walk_N");
+                isWalking = true;
+            }
+        }
+        else
+        {
+            if (isWalking)
+            {
+                animator.Play("Idle");
+                isWalking = false;
+            }
+        }
+
+        if (Vector3.Distance(target.position, transform.position) > 0.1f)
+        {
+            playerIsMoving = true;
+            nav.SetDestination(target.position);
+        }
+        else
+        {
+            playerIsMoving = false;
+        }
+
+        if (!nav.pathPending && !playerIsMoving)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(target.position - transform.position);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * 5f);
+        }
     }
 }
