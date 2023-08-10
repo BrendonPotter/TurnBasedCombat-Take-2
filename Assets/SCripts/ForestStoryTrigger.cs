197b0093381af14447f469b2f797bb8a39c48d9e
An Vo <107334075+truonganvo@users.noreply.github.com>
2023-08-10 16:01:13 +1000

    finalizing

--- Assets/SCripts/ForestStoryTrigger.cs
+++ Assets/SCripts/ForestStoryTrigger.cs
@@ -9,6 +9,10 @@ public class ForestStoryTrigger : MonoBehaviour
     [SerializeField] GameObject mage;
     [SerializeField] GameObject storyCollider;
 
+    //Temporary disable random encounter battle after the player spot the bandit hideout
+    [SerializeField] GameObject disableRandomEncounter;
+
+
     //Village Scene Manager
     [SerializeField] GameObject normalVillage;
     [SerializeField] GameObject burningVillage;
@@ -38,6 +42,8 @@ public class ForestStoryTrigger : MonoBehaviour
 
             normalVillage.SetActive(false);
             burningVillage.SetActive(true);
+
+            disableRandomEncounter.SetActive(false);
 
             // Enable the target game object
             if (targetGameObject != null)
